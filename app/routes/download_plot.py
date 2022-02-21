42544d4ef21d13ae1b7a9bff97004844171207d9
Lucia Pintor <luciapintor90@gmail.com>
2022-02-21 11:56:33 +0100

    with statement used to close io.BytesIO in download_plot.py. issue #2 improvements

--- app/routes/download_plot.py
+++ app/routes/download_plot.py
@@ -15,25 +15,24 @@ def download_plot(job_id=None):
     """Returns a JSON file with the results."""
 
     # data can be kept as bytes in an in-memory buffer
-    output = io.BytesIO()
+    with io.BytesIO() as output:
+        if job_id is not None:
+            task = get_task_from_queue(job_id)
 
-    if job_id is not None:
-        task = get_task_from_queue(job_id)
+            # get the result as json
+            result = dict(task.result)
 
-        # get the result as json
-        result = dict(task.result)
+            # get data from the result of the task
+            labels = list(result.keys())
+            data = list(result.values())
 
-        # get data from the result of the task
-        labels = list(result.keys())
-        data = list(result.values())
+            # create the figure
+            fig = create_figure(labels, data)
 
-        # create the figure
-        fig = create_figure(labels, data)
+            # save the plot in the memory buffer
+            FigureCanvas(fig).print_png(output)
 
-        # save the plot in the memory buffer
-        FigureCanvas(fig).print_png(output)
-
-    return Response(
-        output.getvalue(),
-        mimetype="image/png",
-        headers={"Content-disposition": "attachment; filename=result_plot.png"})
+        return Response(
+            output.getvalue(),
+            mimetype="image/png",
+            headers={"Content-disposition": "attachment; filename=result_plot.png"})
