d9d46d12136796b8bb1e3163066ca7d36d0803ce
Lucia Pintor <luciapintor90@gmail.com>
2022-02-21 11:44:54 +0100

    image_histogram API updated to show an additional plot with 3 sub-plots (one for each channel). issue #4 improvements

--- app/routes/image_histogram.py
+++ app/routes/image_histogram.py
@@ -3,7 +3,7 @@ from flask import render_template
 from app import app
 from app.forms.histogram_form import HistogramForm
 from app.utils.create_folder_if_not_found import create_folder_if_not_found
-from app.utils.make_histogram_plot import make_histogram_plot
+from app.utils.make_histogram_plot import make_histogram_plot, make_histogram_plot_rgb
 from config import Configuration
 
 config = Configuration()
@@ -18,17 +18,12 @@ def image_histogram():
         # get image and convert it as np array
         image_id = form.image.data
 
-        # make the histogram plot
-        plt = make_histogram_plot(image_id)
-
-        # save the plot as figure
-        histograms_directory = 'app/static/histograms'
-        create_folder_if_not_found(histograms_directory)
-        plot_id = '{}_histogram.png'.format(image_id.replace('.JPEG', ''))
-        plt.savefig('{}/{}'.format(histograms_directory, plot_id))
+        # make the histogram plots
+        plot_id = make_histogram_plot(image_id)
+        plot_rgb_id = make_histogram_plot_rgb(image_id)
 
         # if it is a post request, it shows the histogram plot
-        return render_template("histogram_output.html", image_id=image_id, plot_id=plot_id)
+        return render_template("histogram_output.html", image_id=image_id, plot_id=plot_id, plot_rgb_id=plot_rgb_id)
 
     # otherwise, it is a get request and should return the image selector
     return render_template('histogram_select.html', form=form)
--- app/utils/make_histogram_plot.py
+++ app/utils/make_histogram_plot.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from app.utils.create_folder_if_not_found import create_folder_if_not_found
 from ml.classification_utils import fetch_image
 
 
 def make_histogram_plot(image_id):
     """
-    This function returns the histogram plot of an image, given its id.
+    This function returns the id of the histogram plot of an image, given its id.
+    Average between channels is considered for every single pixel.
     """
     img_array = fetch_image(image_id)
     img_array = np.array(img_array.convert('RGB'))
@@ -15,16 +17,70 @@ def make_histogram_plot(image_id):
     vals = img_array.mean(axis=2).flatten()
 
     # assign labels
-    plt.xlabel('pixel value')
+    plt.xlabel('pixel values')
     plt.ylabel('occurrences')
 
+    # clear figure
+    plt.figure().clear()
+
     # assign title
-    plt.title("Histogram of the image")
+    plt.title("Histogram of the image (average of the 3 channels of each pixel)")
 
     plt.plot()
 
     # plot histogram with 255 bins (columns)
-    b, bins, patches = plt.hist(vals, 255)
+    b, bins, patches = plt.hist(vals, 255, color='black')
     plt.xlim([0, 255])
 
-    return plt
+    # get the id of the plot and save it
+    plot_id = '{}_histogram.png'.format(image_id.replace('.JPEG', ''))
+    plt.savefig('{}/{}'.format(get_histogram_folder(), plot_id))
+
+    return plot_id
+
+
+def make_histogram_plot_rgb(image_id):
+    """
+    This function returns the id of the histogram plot of an image, given its id.
+    Channels are plotted separately in different sub-plots.
+    """
+    img_array = fetch_image(image_id)
+    img_array = np.array(img_array.convert('RGB'))
+
+    # channels
+    ch_list = ['red', 'green', 'blue']
+
+
+    # clear figure
+    plt.figure().clear()
+
+    # divide in 3 sub-plots
+    fig, axs = plt.subplots(3, 1, tight_layout=True)
+
+    for ch_id, ch in enumerate(ch_list):
+        # get the channel pixels and convert in 1D array
+        vals = img_array[:, :, ch_id].flatten()
+
+        # plot histogram with 255 bins (columns)
+        b, bins, patches = axs[ch_id].hist(vals, 255, color=ch)
+
+        # set limit for x-axis
+        axs[ch_id].set_xlim([0, 255])
+
+    # assign title and labels
+    plt.suptitle("Histograms of the image divided by color (Red, Green, and Blue)")
+    plt.xlabel('pixel values')
+    axs[1].set_ylabel('occurrences')
+
+    # get the id of the plot and save it
+    plot_rgb_id = '{}_histogram_rgb.png'.format(image_id.replace('.JPEG', ''))
+    plt.savefig('{}/{}'.format(get_histogram_folder(), plot_rgb_id))
+
+    return plot_rgb_id
+
+
+def get_histogram_folder():
+    """ This function returns the name of the folder of the histograms."""
+    histogram_folder = 'app/static/histograms'
+    create_folder_if_not_found(histogram_folder)
+    return histogram_folder
