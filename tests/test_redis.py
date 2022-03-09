840a332469b8d0b04a2a29b192e632cf612344cd
Maura Pintor <maura.pintor@gmail.com>
2022-03-09 18:41:53 +0100

    Update test_redis.py

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -4,9 +4,9 @@ from config import Configuration
 
 
 class TestRedis(unittest.TestCase):
-    """import redis
+    import redis
     config = Configuration()
     redis_url = config.REDIS_URL
     r = redis.from_url(redis_url)
-    r.ping()"""
-    pass
+    r.ping()
+    
